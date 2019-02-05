7383ad9590364e226b5d004ff5f82480510d29f7
Mati <matipacheco@gmail.com>
2019-02-05 00:34:30 -0300

    some code refactor and _game_approach

--- api_test.rb
+++ api_test.rb
@@ -4,30 +4,49 @@ require 'net/http'
 
 require_relative 'models/character'
 
-ts = '10'
-marvel_private_key = ENV['MARVEL_PRIVATE_KEY']
-marvel_public_key  = ENV['MARVEL_PUBLIC_KEY']
-
-request_hash = Digest::MD5.hexdigest ts + marvel_private_key + marvel_public_key
-
-parameters = { :ts => 10, :apikey => marvel_public_key, :hash => request_hash }
-
-uri       = URI('https://gateway.marvel.com/v1/public/characters')
-uri.query = URI.encode_www_form(parameters)
-
-response = Net::HTTP.get_response(uri)
-
-if response.is_a?(Net::HTTPSuccess)
-  results = JSON.parse(response.body)['data']['results']
-
-  characters = []
-
-  results.each do |result|
-    character = Character.new()
-    characters << character.from_json(result.to_json)
+def load_fighters
+  File.foreach('characters_id.txt') do |line|
+    fighter_1, fighter_2 = line.split(',').sample(2)
+    return [fighter_1, fighter_2]
   end
-
-  puts characters
-else
-  raise "API conection error!"
 end
+
+ts = '10'
+private_key = ENV['MARVEL_PRIVATE_KEY']
+public_key  = ENV['MARVEL_PUBLIC_KEY']
+
+def request_hash(ts, private_key, public_key)
+  Digest::MD5.hexdigest ts + private_key + public_key
+end
+
+def parameters(ts, private_key, public_key)
+  {
+    :ts => 10,
+    :limit => 100,
+    :hash => request_hash(ts, private_key, public_key),
+    :apikey => public_key
+  }
+end
+
+fighters = []
+
+load_fighters.each do |fighter|
+  uri       = URI('https://gateway.marvel.com/v1/public/characters/' + fighter)
+  uri.query = URI.encode_www_form(parameters(ts, private_key, public_key))
+  response  = Net::HTTP.get_response(uri)
+
+  if response.is_a?(Net::HTTPSuccess)
+    result = JSON.parse(response.body)['data']['results']
+    puts result
+
+    character = Character.new()
+    fighters << character.from_json(result.to_json)
+
+  else
+    raise "API conection error!"
+  end
+end
+
+fighter_1, fighter_2 = fighters
+
+puts 'Los luchadores son: ' + fighter_1.name.to_s + ' y ' + fighter_2.name.to_s
--- models/character.rb
+++ models/character.rb
@@ -1,9 +1,12 @@
 require 'active_model'
 
+# Character Serializer.
+# Since I could not figure out how to ignore unknown fields,
+# attributes method holds a begin/rescue block in case it crushes.
 class Character
   include ActiveModel::Serializers::JSON
 
-  attr_accessor :id, :name, :description
+  attr_accessor :id, :name, :STR, :HP, :LUCK
 
   def attributes=(hash)
     hash.each do |key, value|
@@ -13,5 +16,13 @@ class Character
         next
       end
     end
+
+    self.STR  = rand(1..100)
+    self.HP   = rand(1..400)
+    self.LUCK = rand(1..10)
+  end
+
+  def validate
+    id.is_a?(Integer) && name.is_a?(String)
   end
 end
